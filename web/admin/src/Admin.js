a69d7d566666f7189872c8bcee16bf8e6d5f4599
Adam Liechty <adam@doubledutch.me>
2018-04-04 17:00:34 -0700

    BigScreen skeleton

---
+++ web/admin/src/Admin.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2018 DoubleDutch, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { PureComponent } from 'react'
+import './Admin.css'
+
+import {mapPushedDataToStateObjects, mapPushedDataToObjectOfStateObjects} from './firebaseHelpers'
+import Questions from './Questions'
+import {openTab} from './utils'
+
+export default class Admin extends PureComponent {
+  state = {
+    sessions: {},
+    questionsBySession: {}
+  }
+
+  sessionsRef = () => this.props.fbc.database.private.adminRef('sessions')
+  questionsRef = () => this.props.fbc.database.private.adminRef('questions')
+
+  componentDidMount() {
+    const {fbc} = this.props
+    mapPushedDataToStateObjects(fbc, this.sessionsRef(), this, 'sessions')
+    mapPushedDataToObjectOfStateObjects(fbc, this.questionsRef(), this, 'questionsBySession', (key, value) => value.sessionId)
+    this.props.fbc.getLongLivedAdminToken().then(longLivedToken => this.setState({longLivedToken}))
+  }
+
+  componentDidUpdate() {
+    if (!this.state.sessionId) {
+      const sessionIds = Object.keys(this.state.sessions)
+      if (sessionIds.length) this.setState({sessionId: sessionIds[0]})
+    }
+  }
+
+  render() {
+    const {launchDisabled, sessionId, sessions} = this.state
+    return (
+      <div className="Admin">
+        <div className="row">
+          <select value={sessionId} onChange={this.onSessionChange}>
+            <option value="">-- Select a session --</option>
+            { Object.values(sessions).map(s => <option key={s.id} value={s.id}>{s.name}</option>) }
+          </select>
+          <button onClick={this.createSession}>Create new session</button>
+        </div>
+        { sessionId && <div>
+            <label className="row">
+              <span>Session Name: </span>
+              <input type="text" value={sessions[sessionId].name} onChange={this.onSessionNameChange} />
+              <button className="secondary" onClick={this.deleteSession}>Delete Session</button>
+              { <button onClick={this.launchPresentation} disabled={launchDisabled || !this.bigScreenUrl()}>Launch Presentation</button> }
+            </label>
+            <Questions
+              questions={Object.values(this.questionsForCurrentSession())}
+              questionsRef={this.questionsRef()}
+              renderFooter={() => (
+                <footer>
+                  <div><label>Time per question: <input type="number" value={sessions[sessionId].secondsPerQuestion} onChange={this.onSecondsChange} /> seconds</label></div>
+                  <div>
+                    <button onClick={this.addQuestion}>Add New Question</button>
+                  </div>
+                </footer>
+              )}
+            />
+          </div>
+        }
+        <iframe className="big-screen-container" src={this.bigScreenUrl()} title="presentation" />
+      </div>
+    )
+  }
+
+  questionsForCurrentSession = () => Object.values(this.state.questionsBySession[this.state.sessionId] || {})
+
+  onSessionChange = e => this.setState({sessionId: e.target.value})
+  onSessionNameChange = e => this.sessionsRef().child(this.state.sessionId).update({name: e.target.value})
+  onSecondsChange = e => this.sessionsRef().child(this.state.sessionId).update({secondsPerQuestion: e.target.value})
+  createSession = () => this.sessionsRef().push({name: 'New Session', secondsPerQuestion: 30}).then(ref => this.setState({sessionId: ref.key}))
+  deleteSession = () => {
+    const {sessionId, sessions} = this.state
+    if (window.confirm(`Are you sure you want to delete session '${sessions[sessionId].name}'?`)) {
+      this.setState({sessionId: ''})
+      this.sessionsRef().child(sessionId).remove()
+    }
+  }
+
+  addQuestion = () => {
+    const {sessionId} = this.state
+    this.questionsRef().push({sessionId, order: this.questionsForCurrentSession().length, text: '', options: ['','','',''], correctIndex: 0})
+  }
+
+  launchPresentation = () => {
+    this.setState({launchDisabled: true})
+    setTimeout(() => this.setState({launchDisabled: false}), 2000)
+    openTab(this.bigScreenUrl())
+  }
+
+  bigScreenUrl = () => this.state.longLivedToken ? `?page=bigScreen&session=${encodeURIComponent(this.state.sessionId)}&token=${encodeURIComponent(this.state.longLivedToken)}` : null
+}
--- web/admin/src/App.js
+++ web/admin/src/App.js
@@ -15,90 +15,38 @@
  */
 
 import React, { PureComponent } from 'react'
-import './App.css'
+import './base.css'
 
 import client from '@doubledutch/admin-client'
 import FirebaseConnector from '@doubledutch/firebase-connector'
-import {mapPushedDataToStateObjects, mapPushedDataToObjectOfStateObjects} from './firebaseHelpers'
-import Questions from './Questions'
+import Admin from './Admin'
+import BigScreen from './BigScreen'
+import {parseQueryString} from './utils'
 const fbc = FirebaseConnector(client, 'trivia')
 
 fbc.initializeAppWithSimpleBackend()
-const sessionsRef = () => fbc.database.private.adminRef('sessions')
-const questionsRef = () => fbc.database.private.adminRef('questions')
 
 export default class App extends PureComponent {
-  state = {
-    sessions: {},
-    questionsBySession: {}
+  constructor() {
+    super()
+    this.state = {}
+    const { token } = parseQueryString()
+    if (token) client.longLivedToken = token
   }
 
   componentDidMount() {
-    fbc.signinAdmin()
-    .then(user => {
-      mapPushedDataToStateObjects(fbc, sessionsRef(), this, 'sessions')
-      mapPushedDataToObjectOfStateObjects(fbc, questionsRef(), this, 'questionsBySession', (key, value) => value.sessionId)
-    })
-  }
-
-  componentDidUpdate() {
-    if (!this.state.sessionId) {
-      const sessionIds = Object.keys(this.state.sessions)
-      if (sessionIds.length) this.setState({sessionId: sessionIds[0]})
-    }
+    fbc.signinAdmin().then(() => this.setState({isSignedIn: true}))
   }
 
   render() {
-    const {sessionId, sessions} = this.state
-    return (
-      <div className="App">
-        <div className="row">
-          <select value={sessionId} onChange={this.onSessionChange}>
-            <option value="">-- Select a session --</option>
-            { Object.values(sessions).map(s => <option key={s.id} value={s.id}>{s.name}</option>) }
-          </select>
-          <button onClick={this.createSession}>Create new session</button>
-        </div>
-        { sessionId && <div>
-            <label className="row">
-              <span>Session Name: </span>
-              <input type="text" value={sessions[sessionId].name} onChange={this.onSessionNameChange} />
-              <button className="secondary" onClick={this.deleteSession}>Delete Session</button>
-            </label>
-            <Questions
-              questions={Object.values(this.questionsForCurrentSession())}
-              questionsRef={questionsRef()}
-              renderFooter={() => (
-                <footer>
-                  <div><label>Time per question: <input type="number" value={sessions[sessionId].secondsPerQuestion} onChange={this.onSecondsChange} /> seconds</label></div>
-                  <div>
-                    <button onClick={this.addQuestion}>Add New Question</button>
-                  </div>
-                </footer>
-              )}
-            />
-          </div>
-        }
-      </div>
-    )
-  }
+    if (!this.state.isSignedIn) return <div>Loading...</div>
+    const qs = parseQueryString()
 
-  questionsForCurrentSession = () => Object.values(this.state.questionsBySession[this.state.sessionId] || {})
-
-  onSessionChange = e => this.setState({sessionId: e.target.value})
-  onSessionNameChange = e => sessionsRef().child(this.state.sessionId).update({name: e.target.value})
-  onSecondsChange = e => sessionsRef().child(this.state.sessionId).update({secondsPerQuestion: e.target.value})
-  createSession = () => sessionsRef().push({name: 'New Session', secondsPerQuestion: 30}).then(ref => this.setState({sessionId: ref.key}))
-  deleteSession = () => {
-    const {sessionId, sessions} = this.state
-    if (window.confirm(`Are you sure you want to delete session '${sessions[sessionId].name}'?`)) {
-      this.setState({sessionId: ''})
-      sessionsRef().child(sessionId).remove()
+    switch (qs.page) {
+      case 'bigScreen':
+        return <BigScreen fbc={fbc} game={qs.game} />
+      default:
+        return <Admin fbc={fbc} />
     }
-  }
-
-  addQuestion = () => {
-    const {sessionId} = this.state
-    questionsRef().push({sessionId, order: this.questionsForCurrentSession().length, text: '', options: ['','','',''], correctIndex: 0})
   }
 }
