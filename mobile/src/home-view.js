efa14f75f3f018e8bbbb3014870be8a7b176bec8
Adam Liechty <adam@doubledutch.me>
2018-05-29 10:20:09 -0700

    Fix null ref

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -113,7 +113,7 @@ export default class HomeView extends PureComponent {
   renderSessions = (sessions, me) => {
     const currentSessions = Object.keys(sessions)
       .map(id => ({...sessions[id], id}))
-      .filter(s => s.state !== 'ENDED' || s.id === me.sessionId)
+      .filter(s => s.state !== 'ENDED' || (me && s.id === me.sessionId))
     if (currentSessions.length === 0) return <View style={s.box}><Text>No trivia games currently. Try back later!</Text></View>
     return (
       <View style={s.box}>
