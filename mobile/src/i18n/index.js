a4ab385bcbf1806c7fab2ff890804ee9ec6772d5
Adam Liechty <adam@doubledutch.me>
2019-02-28 16:08:49 -0800

    translations for arabic, german, spanish, french, italian, japanese, dutch

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,11 +1,30 @@
-import enUS from './en-us/trivia.mobile.json'
-// import deDE from './de-de/trivia.mobile.json'
+import enUS from './en-us/trivia.admin.json'
 
-export default (strings = {
+import arINT from './ar-int/trivia.admin.json'
+import deDE from './de-de/trivia.admin.json'
+import esES from './es-es/trivia.admin.json'
+import frFR from './fr-fr/trivia.admin.json'
+import itIT from './it-it/trivia.admin.json'
+import jaJP from './ja-jp/trivia.admin.json'
+import nlNL from './nl-nl/trivia.admin.json'
+
+export default {
   '': enUS,
   en: enUS,
   'en-US': enUS,
 
-  // "de":    deDE,
-  // "de-DE": deDE,
-})
+  ar: arINT,
+  'ar-INT': arINT,
+  de: deDE,
+  'de-DE': deDE,
+  es: esES,
+  'es-ES': esES,
+  fr: frFR,
+  'fr-FR': frFR,
+  it: itIT,
+  'it-IT': itIT,
+  ja: jaJP,
+  'ja-JP': jaJP,
+  nl: nlNL,
+  'nl-NL': nlNL,
+}
